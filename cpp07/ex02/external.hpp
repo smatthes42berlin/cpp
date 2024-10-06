87cbf03ca67841e5397d942a442c9d47b5bf4adf
smatthes <smatthes@student.42berlin.de>
2024-10-06 10:57:13 +0200

    finished cpp07

---
+++ cpp07/ex02/external.hpp
@@ -0,0 +1,28 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   external.hpp                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: smatthes <smatthes@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/07/21 09:23:07 by smatthes          #+#    #+#             */
+/*   Updated: 2024/10/05 18:03:49 by smatthes         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#pragma once
+
+#include <iomanip>
+#include <iostream>
+#include <string>
+#include <sstream>
+#include <cctype>
+#include <limits>
+#include <climits>
+#include <cfloat>
+#include <stdint.h>
+#include <cstdlib>
+
+// #include <limits>
+// #include <cc_type>
+// #include <cstdlib>
